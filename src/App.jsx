65fc99a0da53f9882934db9748d840b661e2c0c4
Deval Hole <devalhole.pict@gmail.com>
2026-03-08 15:47:16 +0530

    Added MacWindow component with draggable functionality and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import  "./App.scss"
 import Dock from './components/Dock'
 
+import MacWindow from './components/Windows/MacWinodw'
+
 function App() {
 
   return (
   <main>
     <Dock />
+  
+
+    <MacWindow/>
   </main>
   )
 }
